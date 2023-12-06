4fd661e9677152398f6d706d5b6ddcbdea2545cc
Luc Blassel <luc.blassel@gmail.com>
2023-12-06 14:51:47 +0100

    Switching template to Result<T> for functions

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,22 @@
+use anyhow::Result;
+
 #[allow(dead_code)]
 const TEST_1: &str = "";
 const INPUT: &str = include_str!("../../inputs/{{project-name}}.txt");
 
-fn main() {
-    println!("Day XX:");
-    println!("\t1: {}", part_1(INPUT));
-    println!("\t2: {}", part_2(INPUT));
+fn main() -> Result<()> {
+    println!("Day XX");
+    println!("\t1: {}", part_1(INPUT)?);
+    println!("\t2: {}", part_2(INPUT)?);
+
+    Ok(())
 }
 
-fn part_1(input: &str) -> usize {
+fn part_1(input: &str) -> Result<usize> {
     todo!()
 }
 
-fn part_2(input: &str) -> usize {
+fn part_2(input: &str) -> Result<usize> {
     todo!()
 }
 
@@ -22,21 +26,21 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(0, part_1(TEST_1));
+        assert_eq!(0, part_1(TEST_1).unwrap());
     }
 
-    #[test]
-    fn test_input_1() {
-        assert_eq!(0, part_1(INPUT));
-    }
-
-    #[test]
-    fn test_2() {
-        assert_eq!(0, part_2(TEST_1));
-    }
-
-    #[test]
-    fn test_input_2() {
-        assert_eq!(0, part_2(INPUT));
-    }
+    // #[test]
+    // fn test_input_1() {
+    //     assert_eq!(0, part_1(INPUT).unwrap());
+    // }
+    //
+    // #[test]
+    // fn test_2() {
+    //     assert_eq!(0, part_2(TEST_1).unwrap());
+    // }
+    //
+    // #[test]
+    // fn test_input_2() {
+    //     assert_eq!(0, part_2(INPUT).unwrap());
+    // }
 }
